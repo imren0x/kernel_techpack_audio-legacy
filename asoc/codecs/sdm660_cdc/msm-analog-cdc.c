9ca582fc2242b70b66877248e61dbd0ffcffa4ba
Mika <enashinonome264@gmail.com>
2026-06-13 18:37:10 +0000

    techpack: audio: Import remaining Xiaomi changes

--- asoc/codecs/sdm660_cdc/msm-analog-cdc.c
+++ asoc/codecs/sdm660_cdc/msm-analog-cdc.c
@@ -52,7 +52,7 @@
 #define SPK_PMD 2
 #define SPK_PMU 3
 
-#define MICBIAS_DEFAULT_VAL 1800000
+#define MICBIAS_DEFAULT_VAL 2200000
 #define MICBIAS_MIN_VAL 1600000
 #define MICBIAS_STEP_SIZE 50000
 
@@ -2942,6 +2942,7 @@ static int msm_anlg_cdc_lo_dac_event(struct snd_soc_dapm_widget *w,
 			MSM89XX_PMIC_ANALOG_RX_LO_DAC_CTL, 0x08, 0x08);
 		snd_soc_component_update_bits(component,
 			MSM89XX_PMIC_ANALOG_RX_LO_DAC_CTL, 0x40, 0x40);
+		msleep(5);
 		break;
 	case SND_SOC_DAPM_POST_PMU:
 		snd_soc_component_update_bits(component,
